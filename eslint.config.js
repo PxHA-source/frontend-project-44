b5268cd56374eba204539fcfcf2ef2a2c81e9ad0
danii <daniilkulbakov@gmail.com>
2025-04-13 19:48:43 +0300

    eslint не ругается больше

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,31 @@
-import globals from "globals";
-
-import path from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-import pluginJs from "@eslint/js";
-
+/* eslint-disable import/no-extraneous-dependencies, no-underscore-dangle */
+import globals from 'globals';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import babelParser from '@babel/eslint-parser';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended});
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
 
 export default [
-  {languageOptions: { globals: globals.node }},
-  ...compat.extends("airbnb"),
+  {
+    languageOptions: {
+      globals: globals.node,
+      parser: babelParser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        requireConfigFile: false,
+      },
+    },
+    rules: {
+      'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    },
+  },
+  ...compat.extends('airbnb'),
 ];
