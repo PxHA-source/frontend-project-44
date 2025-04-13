872b670a7a34df1570db61de300d163d843acf42
PxHA-source <daniilkulbakov@gmail.com>
2025-04-13 17:33:07 +0300

    Update helper.js

--- src/helper.js
+++ src/helper.js
@@ -1,4 +1,4 @@
-export default function randomNumber(min = 0, max = 100) {
+export default function isRandomNumber(min = 0, max = 100) {
   const r = Math.random() * (max - min) + min;
   return Math.floor(r);
 }
