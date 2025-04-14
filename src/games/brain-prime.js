72e376738510f50c81401cacb2b577dd0d9c9a74
danii <daniilkulbakov@gmail.com>
2025-04-14 14:06:22 +0300

    Изменил имя

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -4,9 +4,9 @@ import cycle from '../index.js';
 
 const descripton = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-const isPrime = (n) => {
-  for (let i = 2; i < n; i += 1) {
-    if (n % i === 0) {
+const isPrime = (number) => {
+  for (let i = 2; i < number; i += 1) {
+    if (number % i === 0) {
       return false;
     }
   }
